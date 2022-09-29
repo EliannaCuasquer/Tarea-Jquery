3629d54bdd87eaa536b8aea6154c721bd13b5cbc
DavidC382 <108535759+daniseb100@users.noreply.github.com>
2022-09-29 17:50:41 +0100

    Instruciones_Elianna

--- js/funciones.js
+++ js/funciones.js
@@ -62,3 +62,27 @@ $( function() {
     state = !state;
   });
 } );
+$( function() {
+  // run the currently selected effect
+  function runEffect() {
+    // get effect type from
+    var selectedEffect = $( "#effectTypes" ).val();
+
+    // Most effect types need no options passed by default
+    var options = {};
+    // some effects have required parameters
+    if ( selectedEffect === "scale" ) {
+      options = { percent: 50 };
+    } else if ( selectedEffect === "size" ) {
+      options = { to: { width: 200, height: 60 } };
+    }
+
+    // Run the effect
+    $( "#effect2" ).toggle( selectedEffect, options, 500 );
+  };
+
+  // Set effect from select menu value
+  $( "#button2" ).on( "click", function() {
+    runEffect();
+  });
+} );
